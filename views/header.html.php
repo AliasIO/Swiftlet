076b77ccdf9b0db21397589c1c7e8183d98c8a21
ElbertF <elbert@tundra.com.au>
2012-02-01 12:50:01 +1100

    Use namespace in header

--- views/header.html.php
+++ views/header.html.php
@@ -1,9 +1,11 @@
+<?php namespace Swiftlet ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-us">
 	<head>
-		<title><?php echo Swiftlet\View::getTitle() ?></title>
+		<title><?php echo View::getTitle() ?></title>
 
-		<link type="text/css" rel="stylesheet" href="<?php echo Swiftlet\App::getRootPath() ?>views/css/layout.css"/>
+		<link type="text/css" rel="stylesheet" href="<?php echo App::getRootPath() ?>views/css/layout.css"/>
 	</head>
 	<body>
