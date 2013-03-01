ab735d0908fd27a51eb02cf6b4d39211912a1954
Elbert Alias <elbert@alias.io>
2013-03-02 10:34:14 +1100

    Switched from XHTML to HTML5

--- views/header.html.php
+++ views/header.html.php
@@ -1,9 +1,9 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
 
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-us">
+<html>
 	<head>
 		<title><?php echo $this->htmlEncode($this->app->getConfig('siteName')) . ' - ' . $this->get('pageTitle') ?></title>
 
-		<link type="text/css" rel="stylesheet" href="<?php echo $this->app->getRootPath() ?>views/css/layout.css"/>
+		<link type="text/css" rel="stylesheet" href="<?php echo $this->app->getRootPath() ?>views/css/layout.css">
 	</head>
 	<body>
